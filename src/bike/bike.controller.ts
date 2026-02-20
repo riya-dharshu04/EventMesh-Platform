afa2f966107ea0715b7c3f97e17b169b7527c84d
Priyadharshini V <priyadharshinimohan04@gmail.com>
2026-02-20 22:41:16 +0530

    fix/registerbikes

--- src/bike/bike.controller.ts
+++ src/bike/bike.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Post, Get, Body } from "@nestjs/common";
+import { Controller, Post, Get, Body, UseGuards,Request } from "@nestjs/common";
 import { RegisterBikeDto } from './bike.dto';
 import { BikesService } from "./bike.service";
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('bikes')
 export class BikesController {
     constructor(private readonly bikesService: BikesService) { }
 
     @Post('register')
-    registerBikes(@Body() dto: RegisterBikeDto) {
-        return this.bikesService.registerBike(dto);
+    @UseGuards(AuthGuard('jwt'))
+    registerBikes(@Body() dto: RegisterBikeDto,@Request() req,) {
+        return this.bikesService.registerBike(dto,req.user.userId);
     }
 
     @Get('list')
